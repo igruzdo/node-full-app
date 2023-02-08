aab7011c419b2fb91720adb8a75647a1116aca34
Igor Gruzdo <igruzdo@yandex.ru>
2023-02-09 00:28:06 +0300

    refactor delete and update method 8-11

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -56,18 +56,38 @@ export const createTour = async (req, res) => {
 	}
 }
 
-export const updateTour = (req, res) => {
-	res.status(200).json({
-		status: 'success',
-		data: {
-			tours: []
-		}
-	})
+export const updateTour = async (req, res) => {
+	try {
+
+		const tour =  await Tour.findByIdAndUpdate(req.params.id, req.body, {
+			new: true
+		})
+
+		res.status(200).json({
+			status: 'success',
+			data: {
+				tour
+			}
+		})
+	} catch(err) {
+		res.status(400).json({
+			status: 'fail',
+			message: 'Invalid data'
+		})
+	}
+
 }
 
-export const deleteTour = (req, res) => {
-	res.status(204).json({
-		status: 'success',
-		data: null
-	})
+export const deleteTour = async (req, res) => {
+	try {
+		await Tour.findByIdAndDelete(req.params.id)
+		res.status(200).json({
+			status: 'success',
+		})
+	} catch(err) {
+		res.status(400).json({
+			status: 'fail',
+			message: 'Invalid data'
+		})
+	}
 }
